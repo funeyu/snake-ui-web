eca8e494b953f288b852f4cc09ac294e59fa306a
funeyu <funeyu@gmail.com>
2020-06-17 08:39:45 +0800

    去掉不用的js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(
       '/api',
       proxy({
-        target: 'http://127.0.0.1:8090/',
+        target: 'http://xiaoshesoso.com/',
         changeOrigin: true,
       })
     );

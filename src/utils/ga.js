5df29f933e609b588d6ba63a089db0f7f5ed898e
funeyu <funeyu@gmail.com>
2020-07-03 19:50:53 +0800

    换百度统计

--- src/utils/ga.js
+++ src/utils/ga.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-166967746-1');
 
 export const traceEvent = function(category, action, label) {
-    if (process.env.NODE_ENV !== 'development') {
-        ReactGA.event({category, action, label});
-    }
+    if (process.env.NODE_ENV !== 'development') {}
 };
 
 export const pageView = function(path) {
-    if (process.env.NODE_ENV !== 'development') {
-        ReactGA.set({page: path});
-        ReactGA.pageview(path);
-    }
+    if (process.env.NODE_ENV !== 'development') {}
 };

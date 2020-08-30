e18d8955a53ef36a27c6258b8720962be6cb5753
funeyu <funeyu@gmail.com>
2020-08-30 23:24:08 +0800

    修改bug

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -4,9 +4,9 @@ import { traceEvent } from 'utils/ga';
 import './index.less';
 
 const Placeholders = {
-  'movies': '全网搜索电影',
-  'tvs': '全网搜索电视剧,追剧,免费看',
-  'animations': '全网搜番,动漫随心看',
+  'movie': '全网搜索电影',
+  'tv': '全网搜索电视剧,追剧,免费看',
+  'animation': '全网搜番,动漫随心看',
   'blog': '搜索千万个人博客，寻找有趣思想',
   'tool': '搜索好用的工具， 让你工作效率嗖嗖的',
   'hot': '搜索热搜榜，这里有互联网人的记忆',
@@ -56,6 +56,7 @@ export default ({keyword='', type})=> {
     const renderDuiGou = function() {
       return <svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>
     }
+    console.log('t', t)
     return (
       <div className='search'>
         <span className='area'>
@@ -64,7 +65,7 @@ export default ({keyword='', type})=> {
             <span className='hidden'>
               <span className='up'></span>
               <span className='one' onClick={()=> setType('movie')}>{t === 'movie' && renderDuiGou()}电影</span>
-              <span className='one' onClick={()=> setType('mv')}>{t === 'mv' && renderDuiGou()}电视</span>
+              <span className='one' onClick={()=> setType('tv')}>{t === 'tv' && renderDuiGou()}电视</span>
               <span className='one' onClick={()=> setType('animation')}>{t === 'animation' && renderDuiGou()}动漫</span>
               <span className='one' onClick={()=> setType('blog')}>{t === 'blog' && renderDuiGou()}博客</span>
             </span>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -17,7 +17,7 @@ export default ()=> {
           <img src={logo} />
         </div>
         <div className='note'>
-          <p>全网搜索免费电影,电视剧,动漫,有趣的个人博客</p>
+          <p>全网搜索免费电影,电视剧,动漫(百万视频资源),有趣的个人博客(千万个人博客)</p>
           <p>www.xiaoshesoso.com 搜罗有趣的小东西</p>
           <p>仅限个人交流学习，搜索的内容来源第三方网站</p>
         </div>
